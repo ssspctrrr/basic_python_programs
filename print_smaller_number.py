7c1141f84e3b382e46566c231b343f02662f439d
Leo Caldosa <caldosaleo@gmail.com>
2025-03-11 23:23:12 +0800

    pseudocode

---
+++ print_smaller_number.py
@@ -0,0 +1,11 @@
+# define numbers array
+
+# create loop for 2 iterations
+    # input number and append the number to the numbers array
+
+# if first_number less than second_number
+    # print first_number
+# else if second_number less than first_number
+    # print second_number
+# else 
+    #print "Numbers are equal"
