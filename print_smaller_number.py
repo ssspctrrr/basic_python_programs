2ca656ae14c05a0c3fc18ac44ee6984e0437d33a
Leo Caldosa <caldosaleo@gmail.com>
2025-03-11 23:50:38 +0800

    input number twice

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,7 +1,13 @@
 # define numbers array
+numbers = []
 
 # create loop for 2 iterations
+
+for i in range(1, 3):
+    
     # input number and append the number to the numbers array
+
+    number = int(input(f"Enter number {i}: "))
 
 # if first_number less than second_number
     # print first_number
