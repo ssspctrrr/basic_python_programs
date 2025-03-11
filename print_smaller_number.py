c2c0ece0b333e21e17eff5f00aad13561b0b7546
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 00:02:17 +0800

    print smaller number or if equal

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -7,11 +7,15 @@ for i in range(1, 3):
     # input number and append the number to the numbers array
     numbers.append(int(input(f"Enter number {i}: ")))
 
-print(numbers)
-
 # if first_number less than second_number
+if numbers[0] < numbers[1]:
     # print first_number
+    print(numbers[0])
 # else if second_number less than first_number
+elif numbers[1] < numbers[0]:
     # print second_number
+    print(numbers[1])
 # else 
+else:
     #print "Numbers are equal"
+    print("Numbers are equal.")
