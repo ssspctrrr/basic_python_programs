3c792977c3038555a7b7cc0cd03ef9615a1477b7
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 22:19:10 +0800

    print not equal

--- check_numbers_not_equal.py
+++ check_numbers_not_equal.py
@@ -6,7 +6,7 @@ for i in range(1, 3):
     # input and append numbers to numbers array
     numbers.append(int(input(f"Enter number {i}: ")))
 
-print(numbers)
-
 # check if first_number is not equal to second_number
+if numbers[0] != numbers[1]:
     # print "Not equal"
+    print("Not equal")
