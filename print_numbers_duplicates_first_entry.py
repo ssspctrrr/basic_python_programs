c53a4750ffc8b0152b309407941ac66bc14f673c
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 00:51:51 +0800

    input number every iteration

--- print_numbers_duplicates_first_entry.py
+++ print_numbers_duplicates_first_entry.py
@@ -1,7 +1,10 @@
 # define numbers array
+numbers = []
 
 # loop 10 iterations
+for i in range(1, 11):
     # input number
+    number = int(input(f"Enter number {i}: "))
     # check if number is not yet in numbers
         # append to numbers if true
 
