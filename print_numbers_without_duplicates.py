20d1bb9b7cd0d0ebcda84a75086a4cd6e70e57d4
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 00:37:37 +0800

    pseudocode

---
+++ print_numbers_without_duplicates.py
@@ -0,0 +1,8 @@
+# define numbers array
+
+# loop 10 iterations
+    # input number and append to numbers array
+
+# iterate over the values of numbers
+    # check if current value is unique in numbers
+        # print if true
