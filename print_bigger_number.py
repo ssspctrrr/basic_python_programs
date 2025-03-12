f12aa6a0bb3e09413647c1dc90dc2fae2bdcf06e
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 21:58:24 +0800

    print bigger number or equal

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -6,11 +6,15 @@ for i in range(1, 3):
     # input number and append input to numbers array
     numbers.append(int(input(f"Enter number {i}: ")))
 
-print(numbers)
-
 # check if first_number is greater than second_number
+if numbers[0] > numbers[1]:
     # print first_number 
+    print(numbers[0])
 # else if second_number is greater than first_number
+elif numbers[1] > numbers[0]:
     # print second_number 
+    print(numbers[1])
 # else
-    # prit "Equal"
+else:
+    # print "Numbers are equal"
+    print("Nubers are equal.")
