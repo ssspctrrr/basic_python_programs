5ee26f351911450514e0ed094bf8cdf755f8508c
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 17:13:12 +0800

    print quotient

--- divide_with_decimal_point.py
+++ divide_with_decimal_point.py
@@ -6,5 +6,5 @@ for i in range(1, 3):
     # input number and append to numbers array
     numbers.append(int(input(f"Enter number {i}: ")))
 
-# divide first_number by second_number
-# print quotient formatted with decimal point
+# divide first_number by second_number; print quotient formatted with decimal point
+print(numbers[0] / numbers[1])
