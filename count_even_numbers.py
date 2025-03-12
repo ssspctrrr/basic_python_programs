ef4eefafc2c580e3492e4e84bc4de8addb295827
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 23:25:15 +0800

    input number in 10 iterations

--- count_even_numbers.py
+++ count_even_numbers.py
@@ -1,7 +1,11 @@
 # define count_even variable
+count_even = 0
 
 # loop 10 iterations
+for i in range(1, 11):
     # input number
+    number = int(input(f"Enter number {i}: "))
+    print(number)
 
     # check if number is even
         # add 1 to count_even if true
