43eac2f323e35b7dda700568cce33feba014fbcd
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 22:45:03 +0800

    input and append to array twice

--- divide_without_decimal_point.py
+++ divide_without_decimal_point.py
@@ -1,6 +1,11 @@
 # define numbers array
+numbers = []
 
 # loop 2 iterations
+for i in range(1, 3):
     # input and append number to numbers array
+    numbers.append(int(input(f"Enter number {i}: ")))
+
+print(numbers)
 
 # print first_number divided by second_number without decimal point
