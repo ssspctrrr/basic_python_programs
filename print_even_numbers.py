c26172b3bec53a06f59a3e3a93448853a8ce9056
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 21:22:40 +0800

    print even numbers

--- print_even_numbers.py
+++ print_even_numbers.py
@@ -1,6 +1,7 @@
 # print number 0 to 100
 for i in range(0, 101):
-    print(i)
 
-# check if number is even
+    # check if number is even
+    if i % 2 == 0:
     # print number if true
+        print(i)
