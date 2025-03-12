e86a75990d5e4d4203b17500448362b2e2d8f27e
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 00:40:48 +0800

    input and append to array every iteration

--- print_numbers_without_duplicates.py
+++ print_numbers_without_duplicates.py
@@ -1,7 +1,10 @@
 # define numbers array
+numbers = []
 
 # loop 10 iterations
+for i in range(1, 11):
     # input number and append to numbers array
+    numbers.append(int(input(f"Enter numbers {i}: ")))
 
 # iterate over the values of numbers
     # check if current value is unique in numbers
