7ed97fe54ef901743b4b63533c6f9df0c6d5f1d1
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 22:57:11 +0800

    input and append it to array

--- remainder_calculator.py
+++ remainder_calculator.py
@@ -1,6 +1,9 @@
 # define numbers array
+numbers = []
 
 # loop 2 iterations
+for i in range(1, 3):
     # input and append it to numbers array
+    numbers.append(int(input(f"Enter number {i}: ")))
 
 # print remainder of first_number divided by second_number
