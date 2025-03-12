cac8fe48a2f1b516d0b20def2cccc56a9ffa37b4
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 14:21:19 +0800

    input number twice

--- multiply_2_numbers.py
+++ multiply_2_numbers.py
@@ -1,7 +1,10 @@
 # define numbers array
+numbers = []
 
 # loop for 2 iterations
+for i in range(1, 3):
     # input numbers and append to numbers array
+    number = int(input(f"Enter number {i}: "))
 
 # multiply the numbers
 # print product
