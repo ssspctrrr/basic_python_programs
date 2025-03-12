e12272e7430512233497501a93e8c89a8c1732dc
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 23:05:19 +0800

    loop 10 iterations

--- first_number_minus_rest.py
+++ first_number_minus_rest.py
@@ -1,6 +1,9 @@
 # define numbers array
+numbers = 0
 
 # loop 10 iterations
+for i in range(1, 11):
+    print(i)
     # input number and append to numbers array
 
 # add the second_number to tenth_number altogether
