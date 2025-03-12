41a992d867b0c7ae74b48afd13b74794dd76ba70
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 23:04:29 +0800

    pseudocode

---
+++ first_number_minus_rest.py
@@ -0,0 +1,7 @@
+# define numbers array
+
+# loop 10 iterations
+    # input number and append to numbers array
+
+# add the second_number to tenth_number altogether
+# print first_number subtracted by the sum
