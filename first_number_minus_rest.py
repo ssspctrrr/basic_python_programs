94c9783c8fc03c8fb8008a4bd63ab92f513961f4
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 23:17:30 +0800

    print first number minus remaining numbers

--- first_number_minus_rest.py
+++ first_number_minus_rest.py
@@ -6,5 +6,5 @@ for i in range(1, 11):
     # input number and append to numbers array
     numbers.append(int(input(f"Enter number {i}: ")))
 
-# add the second_number to tenth_number altogether
-# print first_number subtracted by the sum
+# print the first_number minus the sum of the second_number to tenth_number altogether
+print(numbers[0] - sum(numbers[1:]))
