843abe25e7d56d4a5192cb5c6af3b7307a1b89eb
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 23:54:37 +0800

    input number and append to array twice

--- print_numbers_in_between.py
+++ print_numbers_in_between.py
@@ -1,6 +1,9 @@
 # define numbers array
+numbers = []
 
 # loop 2 iterations
+for i in range(1, 3):
     # input number and append it to numbers array
+    numbers.append(int(input(f"Enter number {i}: ")))
 
 # print list of numbers between first_number and second_number
