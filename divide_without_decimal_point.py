f4426b9cc9527fba5d97f210ab102e5e6212edfe
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 22:47:54 +0800

    print quotient w/o decimal point

--- divide_without_decimal_point.py
+++ divide_without_decimal_point.py
@@ -6,6 +6,5 @@ for i in range(1, 3):
     # input and append number to numbers array
     numbers.append(int(input(f"Enter number {i}: ")))
 
-print(numbers)
-
 # print first_number divided by second_number without decimal point
+print(numbers[0] // numbers[1])
