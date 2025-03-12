e835bbedd4c01880e89daecc9b2d118bc63b3768
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 22:28:51 +0800

    input and append to array twice

--- subtract_2_numbers.py
+++ subtract_2_numbers.py
@@ -1,7 +1,10 @@
 # define numbers array
+numbers = []
 
 # loop 2 iterations
+for i in range(1, 3):
     # input and append number to numbers array
+    numbers.append(int(input(f"Enter number {i}: ")))
 
 # subtract first_number by second_number
 # print result
