02f854fe612a22b0bf403a665ff59f95741853cc
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 20:37:01 +0800

    loop 10 iterations

--- sum_10_numbers.py
+++ sum_10_numbers.py
@@ -1,7 +1,10 @@
 # define numbers array
+numbers = []
 
 # loop for 10 iterations
+for i in range(1, 11):
     # input and append number to numbers array
+    print(i)
 
 # sum first_number to tenth_number
 # print the sum
