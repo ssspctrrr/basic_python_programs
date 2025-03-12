2bcd391502849a3c3e3c4feee62973bb9aa16795
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 17:09:04 +0800

    input and append number to array

--- divide_with_decimal_point.py
+++ divide_with_decimal_point.py
@@ -1,7 +1,10 @@
 # define numbers array
+numbers = []
 
 # loop for 2 iterations
+for i in range(1, 3):
     # input number and append to numbers array
+    numbers.append(int(input(f"Enter number {i}: ")))
 
 # divide first_number by second_number
 # print quotient formatted with decimal point
