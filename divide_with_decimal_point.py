7f4870c8731c2fa31f86116ee3e06dc143940e70
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 17:05:35 +0800

    pseudocode

---
+++ divide_with_decimal_point.py
@@ -0,0 +1,7 @@
+# define numbers array
+
+# loop for 2 iterations
+    # input number and append to numbers array
+
+# divide first_number by second_number
+# print quotient formatted with decimal point
