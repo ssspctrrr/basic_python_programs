cd5aeb6bfddebfd8af6c39f6870e2106c7745d4b
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 23:38:41 +0800

    print odd numbers

--- print_odd_numbers.py
+++ print_odd_numbers.py
@@ -3,8 +3,10 @@ current_number = 0
 
 # print 0 - 100
 while current_number <= 100:
-    print(current_number)
-    current_number += 1
     
     # check if current_number is odd
+    if current_number % 2 == 1:
         # print current_number if true
+        print(current_number)
+
+    current_number += 1
