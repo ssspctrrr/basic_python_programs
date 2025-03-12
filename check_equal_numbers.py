1294fb760dd58132f567cc03caa3452f2fd08613
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 13:30:53 +0800

    input number twice

--- check_equal_numbers.py
+++ check_equal_numbers.py
@@ -1,7 +1,10 @@
 # define numbers array
+numbers = []
 
 # loop for 2 iterations
+for i in range(1, 3):
     # input the numbers and append into the numbers array
+    number = int(input(f"Enter number {i}: "))
 
 # if first_number is equal to second_number
     # print "Equal"
