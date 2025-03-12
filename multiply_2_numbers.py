cec8e031b7c1a97785c9033a166458037f34d009
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 14:25:42 +0800

    print product

--- multiply_2_numbers.py
+++ multiply_2_numbers.py
@@ -6,5 +6,5 @@ for i in range(1, 3):
     # input numbers and append to numbers array
     numbers.append(int(input(f"Enter number {i}: ")))
 
-# multiply the numbers
-# print product
+# print the product of the numbers
+print(numbers[0] * numbers[1])
