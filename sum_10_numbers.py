ae416707f8eccbba1fe0d797622f694a42da7aba
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 20:38:25 +0800

    input and append number to array

--- sum_10_numbers.py
+++ sum_10_numbers.py
@@ -4,7 +4,9 @@ numbers = []
 # loop for 10 iterations
 for i in range(1, 11):
     # input and append number to numbers array
-    print(i)
+    numbers.append(int(input(f"Enter number {i}: ")))
+
+print(numbers)
 
 # sum first_number to tenth_number
 # print the sum
