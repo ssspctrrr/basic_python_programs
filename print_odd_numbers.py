e7c378288b9a87a6ef87de64cc144103fce48207
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 23:37:08 +0800

    print 0-100

--- print_odd_numbers.py
+++ print_odd_numbers.py
@@ -1,5 +1,10 @@
 # define current_number variable
+current_number = 0
 
 # print 0 - 100
+while current_number <= 100:
+    print(current_number)
+    current_number += 1
+    
     # check if current_number is odd
         # print current_number if true
