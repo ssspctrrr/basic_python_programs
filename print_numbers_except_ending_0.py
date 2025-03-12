94df4c50d0a3af1a2c3f7ae5abcd461e2b70880d
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 21:32:57 +0800

    print output

--- print_numbers_except_ending_0.py
+++ print_numbers_except_ending_0.py
@@ -1,6 +1,7 @@
 # print 0 - 100
 for i in range(0, 101):
-    print(i)
 
-# check if number does not end in 0
-    # print if true
+    # check if number does not end in 0
+    if i % 10 != 0:
+        # print if true
+        print(i)
