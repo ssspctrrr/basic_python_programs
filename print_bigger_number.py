218325ba70e0b96b081988a6f4fa128d4fddd4c0
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 21:43:03 +0800

    pseudocode

---
+++ print_bigger_number.py
@@ -0,0 +1,12 @@
+# define numbers array
+
+# loop two iterations
+    # input number
+    # append input to numbers array
+
+# check if first_number is greater than second_number
+    # print first_number 
+# else if second_number is greater than first_number
+    # print second_number 
+# else
+    # prit "Equal"
