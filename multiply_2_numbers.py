dcf4447027fb028fb7ebf1ead7b1c881d1f2ce73
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 14:22:47 +0800

    append inputs to array

--- multiply_2_numbers.py
+++ multiply_2_numbers.py
@@ -4,7 +4,7 @@ numbers = []
 # loop for 2 iterations
 for i in range(1, 3):
     # input numbers and append to numbers array
-    number = int(input(f"Enter number {i}: "))
+    numbers.append(int(input(f"Enter number {i}: ")))
 
 # multiply the numbers
 # print product
