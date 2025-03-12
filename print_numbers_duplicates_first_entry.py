514f97f2b959c05fd68c119ea1f75c31da504ec9
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 00:55:59 +0800

    append inputs unless duplicate and print

--- print_numbers_duplicates_first_entry.py
+++ print_numbers_duplicates_first_entry.py
@@ -5,7 +5,11 @@ numbers = []
 for i in range(1, 11):
     # input number
     number = int(input(f"Enter number {i}: "))
+
     # check if number is not yet in numbers
+    if number not in numbers:
         # append to numbers if true
+        numbers.append(number)
 
 # print numbers array
+print(numbers)
