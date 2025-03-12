2b2cf58ff7c7bbf32801b9c094db26c72f497d30
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 22:17:25 +0800

    input number and append to array

--- check_numbers_not_equal.py
+++ check_numbers_not_equal.py
@@ -1,7 +1,12 @@
 # define numbers array
+numbers = []
 
 # loop two iterations
+for i in range(1, 3):
     # input and append numbers to numbers array
+    numbers.append(int(input(f"Enter number {i}: ")))
+
+print(numbers)
 
 # check if first_number is not equal to second_number
     # print "Not equal"
