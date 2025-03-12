06dfc43b3d27c111d9d0682d94fc03b6a9a5d83a
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 21:03:54 +0800

    input number 10 times

--- count_odd_numbers.py
+++ count_odd_numbers.py
@@ -1,7 +1,10 @@
 # define count_odds variable
+count_odds = 0
 
 # loop 10 iterations
+for i in range(1, 11):
     # input number
+    number = int(input(f"Enter number {i}: "))
 
     # check if number is odd
         # add 1 to count_odds if true
