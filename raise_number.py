261f59e093e0f8067561c1f0acc61faaa7a708b0
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 20:15:29 +0800

    input and append number to array

--- raise_number.py
+++ raise_number.py
@@ -1,6 +1,11 @@
 # define numbers array 
+numbers = []
 
 # loop for two iterations
+for i in range(1, 3):
     # input number and append to numbers array
+    numbers.append(int(input(f"Enter number {i}: ")))
+
+print(numbers)
 
 # print first_number raise to second number
