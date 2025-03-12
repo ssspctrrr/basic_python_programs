81fe414acc626d2cff25039d96ff20988e37fecc
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 21:48:01 +0800

    input number twice

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -1,7 +1,10 @@
 # define numbers array
+numbers = []
 
 # loop two iterations
+for i in range(1, 3):
     # input number
+    number = int(input(f"Enter number {i}: "))
     # append input to numbers array
 
 # check if first_number is greater than second_number
