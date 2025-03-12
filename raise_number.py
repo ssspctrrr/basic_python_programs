9475d5251b4b9699e424182b316f1d924e842f87
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 20:20:39 +0800

    print exponentiation result

--- raise_number.py
+++ raise_number.py
@@ -6,6 +6,5 @@ for i in range(1, 3):
     # input number and append to numbers array
     numbers.append(int(input(f"Enter number {i}: ")))
 
-print(numbers)
-
 # print first_number raise to second number
+print(numbers[0] ** numbers[1])
