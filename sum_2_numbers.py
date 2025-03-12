c5854d6fcb2f52127f4eae23bd31f4dfa549406a
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 13:58:06 +0800

    add and print sum

--- sum_2_numbers.py
+++ sum_2_numbers.py
@@ -6,5 +6,5 @@ for i in range(1, 3):
     # input number and append to numbers array
     numbers.append(int(input(f"Enter number {i}: ")))
 
-# add the two numbers
-# print the sum
+# add the two numbers and print the sum
+print(numbers[0] + numbers[1])
