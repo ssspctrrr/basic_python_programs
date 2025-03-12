ecfd5f819801ade69ed09c800193cb8c46a60c22
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 23:11:48 +0800

    input and append it to array every iteration

--- first_number_minus_rest.py
+++ first_number_minus_rest.py
@@ -1,10 +1,10 @@
 # define numbers array
-numbers = 0
+numbers = []
 
 # loop 10 iterations
 for i in range(1, 11):
-    print(i)
     # input number and append to numbers array
+    numbers.append(int(input(f"Enter number {i}: ")))
 
 # add the second_number to tenth_number altogether
 # print first_number subtracted by the sum
