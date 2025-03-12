4bfc986670f386e2b9078373363c56946303e0e9
Leo Caldosa <caldosaleo@gmail.com>
2025-03-12 13:56:22 +0800

    append inputs to array

--- sum_2_numbers.py
+++ sum_2_numbers.py
@@ -4,7 +4,7 @@ numbers = []
 # loop for 2 iterations
 for i in range(1, 3):
     # input number and append to numbers array
-    number = int(input(f"Enter number {i}: "))
+    numbers.append(int(input(f"Enter number {i}: ")))
 
 # add the two numbers
 # print the sum
