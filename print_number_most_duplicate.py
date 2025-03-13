6a8b719d460c8e6419479ac40d15f4e486d4ee36
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 14:17:55 +0800

    pseudocode

---
+++ print_number_most_duplicate.py
@@ -0,0 +1,15 @@
+# define numbers array
+# define count_duplicate
+# define number_most_duplicate variable
+
+# try if input is valid
+    # loop while input is valid
+        # input number
+        # append input to numbers array
+
+        # check if the inputted number has more duplicates than current count_duplicate
+            # set the inputted number as number_most_duplicate if true
+            # set the count of inputted number in numbers array as count_duplicate
+
+# catch error if input is invalid
+    # print number_most_duplicate 
