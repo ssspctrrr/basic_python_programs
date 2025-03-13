c1b124050b0bce547378d4b9826fc88b8b9ee353
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 15:03:45 +0800

    pseudocode

---
+++ print_highest_to_lowest.py
@@ -0,0 +1,9 @@
+# define numbers array
+
+# try if input is valid
+    # loop while input is valid
+        # input number and append to numbers
+
+# catch error if input is invalid
+    # sort numbers highest to lowest
+    # print sorted numbers
