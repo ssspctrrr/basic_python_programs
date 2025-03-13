d21911e7656f4a4842f8b631299d54c9ac421dbf
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 12:05:33 +0800

    check lowest number and print

--- print_lowest_number.py
+++ print_lowest_number.py
@@ -9,9 +9,11 @@ try:
         number = int(input("Enter number: "))
 
         # check if lowest_number is still None or number is less than lowest_number
+        if lowest_number is None or number < lowest_number:
             # assign lowest_number as the value of number
+            lowest_number = number
 
 #catch error if input is invalid
 except:
     # print lowest_number
-    print(number)
+    print(lowest_number)
