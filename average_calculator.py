08913e6377afe348227510c6f8fced1c37d10562
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 15:18:49 +0800

    check if valid input

--- average_calculator.py
+++ average_calculator.py
@@ -1,9 +1,14 @@
 # define numbers array
+numbers = []
 
 # try if input is value
+try:
     # loop while input is valid
         # input number and append to numbers
+    number = int(input("Enter number: "))
 
 # catch error if input is invalid
+except:
+    print("invalid")
     # get average of numbers
     # print average
