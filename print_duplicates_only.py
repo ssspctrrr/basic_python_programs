1556a56322ae8f85915586cd23c7fee955f96fc7
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 13:24:58 +0800

    pseudocode

---
+++ print_duplicates_only.py
@@ -0,0 +1,12 @@
+# define unique_numbers array
+# define duplicate_numbers array
+
+# loop for 10 iterations
+    # input number
+
+    # check if number is unique
+        # append number to unique_numbers if true
+    # else check if number is not yet in duplicate_numbers
+        # append number to duplicate_numbers if true
+
+# print duplicate_numbers
