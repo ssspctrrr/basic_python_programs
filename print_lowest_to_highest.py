e99825a76f5ec7b965895aee4096249ac61daae5
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 12:52:02 +0800

    loop and append input to array

--- print_lowest_to_highest.py
+++ print_lowest_to_highest.py
@@ -4,8 +4,9 @@ numbers = []
 # try if input is invalid
 try:
     # loop while input is invalid
+    while True:
         # input number and append to numbers array
-    number = int(input("Enter number: "))
+        numbers.append(int(input("Enter number: ")))
 
 # catch error if input is invalid
 except:
