d4a4e0e715feaf5781b88875c7e709a6643ef678
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 17:22:58 +0800

    pseudocode

---
+++ print_highest_number.py
@@ -0,0 +1,11 @@
+# define highest_number variable
+
+# try if input is valid
+    # loop while input is valid
+        # input number
+
+    # check if highest_number is not None or if input greater than current highest_number
+        # assign higest_number as number if true
+
+# catch error if input is invalid
+    # print highest_number 
