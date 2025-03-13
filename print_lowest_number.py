327d65591ca3d97e05fbbad59e27d48413d7c695
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 12:00:49 +0800

    check if input valid

--- print_lowest_number.py
+++ print_lowest_number.py
@@ -2,13 +2,16 @@
 lowest_number = None
 
 # try if input is valid
+try:
     # loop while input is valid
-while True:
+    while True:
     # input number
-    number = int(input("Enter number: "))
+        number = int(input("Enter number: "))
 
         # check if lowest_number is still None or number is less than lowest_number
             # assign lowest_number as the value of number
 
 #catch error if input is invalid
+except:
     # print lowest_number
+    print(number)
