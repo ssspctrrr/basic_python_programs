45e99f14849d4588f61c6eb77c3bc1bce983bdeb
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 17:27:32 +0800

    loop, check highest number and print

--- print_highest_number.py
+++ print_highest_number.py
@@ -4,13 +4,16 @@ highest_number = None
 # try if input is valid
 try:
     # loop while input is valid
+    while True:
         # input number
-    number = int(input("Enter number: "))
+        number = int(input("Enter number: "))
 
-    # check if highest_number is not None or if input greater than current highest_number
-        # assign higest_number as number if true
+        # check if highest_number is not None or if input greater than current highest_number
+        if highest_number is None or number > highest_number:
+            # assign highest_number as number if true
+            highest_number = number
 
 # catch error if input is invalid
 except:
-    print("Invalid")
-    # print highest_number 
+    # print highest_number
+    print(highest_number)
