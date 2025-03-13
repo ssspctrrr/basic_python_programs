356fe6c7c829961e1a2c13c6916da4f3e622a14b
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 13:31:51 +0800

    store unique numbers

--- print_duplicates_only.py
+++ print_duplicates_only.py
@@ -9,8 +9,12 @@ for i in range(1, 11):
     number = int(input(f"Enter number {i}: "))
 
     # check if number is unique
+    if number not in unique_numbers:
         # append number to unique_numbers if true
+        unique_numbers.append(number)
     # else check if number is not yet in duplicate_numbers
         # append number to duplicate_numbers if true
+
+print(unique_numbers)
 
 # print duplicate_numbers
