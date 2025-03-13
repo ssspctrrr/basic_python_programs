1cff6d9f4868333cc362c119aa8154f3ca20b143
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 12:50:42 +0800

    check if valid input

--- print_lowest_to_highest.py
+++ print_lowest_to_highest.py
@@ -1,9 +1,14 @@
 # define numbers array
+numbers = []
 
 # try if input is invalid
+try:
     # loop while input is invalid
         # input number and append to numbers array
+    number = int(input("Enter number: "))
 
 # catch error if input is invalid
+except:
+    print("invalid")
     # sort numbers array
     # print sorted_numbers
