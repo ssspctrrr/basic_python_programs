4b77adf20eacbd0fd625b296fc0ce1efdcdd92cf
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 14:28:57 +0800

    loop and append inputs to array

--- print_number_most_duplicate.py
+++ print_number_most_duplicate.py
@@ -8,9 +8,11 @@ number_most_duplicate = 0
 # try if input is valid
 try:
     # loop while input is valid
-    number = int(input("Enter number: "))
+    while True:
         # input number
+        number = int(input("Enter number: "))
         # append input to numbers array
+        numbers.append(number)
 
         # check if the inputted number has more duplicates than current count_duplicate
             # set the inputted number as number_most_duplicate if true
@@ -18,5 +20,5 @@ try:
 
 # catch error if input is invalid
 except:
-    print("invalid")
+    print(numbers)
     # print number_most_duplicate 
