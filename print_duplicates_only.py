772677db92726c626ae41bbe4e82bb6b35620067
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 13:27:56 +0800

    loop and input every iteration

--- print_duplicates_only.py
+++ print_duplicates_only.py
@@ -1,8 +1,12 @@
 # define unique_numbers array
+unique_numbers = []
 # define duplicate_numbers array
+duplicate_numbers = []
 
 # loop for 10 iterations
+for i in range(1, 11): 
     # input number
+    number = int(input(f"Enter number {i}: "))
 
     # check if number is unique
         # append number to unique_numbers if true
