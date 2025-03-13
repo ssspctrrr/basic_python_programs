6136273237584c0a1d2df5d442343cd358f873f1
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 15:17:11 +0800

    pseudocode

---
+++ average_calculator.py
@@ -0,0 +1,9 @@
+# define numbers array
+
+# try if input is value
+    # loop while input is valid
+        # input number and append to numbers
+
+# catch error if input is invalid
+    # get average of numbers
+    # print average
