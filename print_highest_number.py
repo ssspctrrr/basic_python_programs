354f0fba96bd53c815640df2ac1dddf62e63ed34
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 17:24:44 +0800

    check if valid input

--- print_highest_number.py
+++ print_highest_number.py
@@ -1,11 +1,16 @@
 # define highest_number variable
+highest_number = None
 
 # try if input is valid
+try:
     # loop while input is valid
         # input number
+    number = int(input("Enter number: "))
 
     # check if highest_number is not None or if input greater than current highest_number
         # assign higest_number as number if true
 
 # catch error if input is invalid
+except:
+    print("Invalid")
     # print highest_number 
