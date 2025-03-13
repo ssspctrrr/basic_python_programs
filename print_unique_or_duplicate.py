1cd822c75189c2f18a9e4a846d2ee3bf11b7631e
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 10:58:24 +0800

    pseudocode

---
+++ print_unique_or_duplicate.py
@@ -0,0 +1,14 @@
+# define numbers array
+
+# loop while input is valid
+    # try if input is valid
+        # input number
+
+        # check if number is in numbers array
+            # print "Unique" if true
+            # append to numbers if true
+        # else if number is not in numbers array
+            # print "Duplicate"
+
+    # catch error if input is invalid
+        # break loop
