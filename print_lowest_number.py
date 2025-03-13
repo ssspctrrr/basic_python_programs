538d0e516efb0d77a556fd41083eef5efc15c741
Leo Caldosa <caldosaleo@gmail.com>
2025-03-13 11:46:06 +0800

    pseudocode

---
+++ print_lowest_number.py
@@ -0,0 +1,11 @@
+# define lowest_number variable as None
+
+# try if input is valid
+    # loop while input is valid
+        # input number
+
+        # check if lowest_number is still None or number is less than lowest_number
+            # assign lowest_number as the value of number
+
+#catch error if input is invalid
+    # print lowest_number
